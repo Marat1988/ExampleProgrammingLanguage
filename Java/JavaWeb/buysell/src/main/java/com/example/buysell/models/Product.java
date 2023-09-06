6a73eb2277c241d37463d138685485de6b484fb4
Marat <108996479+Marat1988@users.noreply.github.com>
2023-09-06 18:27:49 +0200

    UPDATE

--- Java/JavaWeb/buysell/src/main/java/com/example/buysell/models/Product.java
+++ Java/JavaWeb/buysell/src/main/java/com/example/buysell/models/Product.java
@@ -7,6 +7,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Entity
 @Table(name = "products")
@@ -28,4 +32,18 @@ public class Product {
     private String city;
     @Column(name = "author")
     private String author;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "product")
+    private List<Image> images  = new ArrayList<>();
+    private Long previewImageId;
+    private LocalDateTime dateOfCreated;
+
+    @PrePersist
+    private void init(){
+        dateOfCreated = LocalDateTime.now();
+    }
+
+    public void addImageToProduct(Image image){
+        image.setProduct(this);
+        images.add(image);
+    }
 }
