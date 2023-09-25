86920883f87267daf15a60625f7ec02ac7fcd170
Marat <pirat03071988@mail.ru>
2023-09-25 23:51:00 +0200

    1

--- JavaAndroid/Messenger/app/src/main/java/com/example/messenger/CreateMessageActivity.java
+++ JavaAndroid/Messenger/app/src/main/java/com/example/messenger/CreateMessageActivity.java
@@ -2,17 +2,26 @@ package com.example.messenger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends AppCompatActivity {
+
+    private EditText editTextMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
+        editTextMsg = findViewById(R.id.editTextMessage);
     }
 
     public void onClickSendMessage(View view) {
+        String msg = editTextMsg.getText().toString();
+        Intent intent = new Intent(this, ReceivedMessageActivity.class);
+        intent.putExtra("msg", msg);
+        startActivity(intent);
     }
 }
--- JavaAndroid/Messenger/app/src/main/java/com/example/messenger/ReceivedMessageActivity.java
+++ JavaAndroid/Messenger/app/src/main/java/com/example/messenger/ReceivedMessageActivity.java
@@ -2,13 +2,21 @@ package com.example.messenger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReceivedMessageActivity extends AppCompatActivity {
+
+    private TextView textViewReceivedMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_received_message);
+        textViewReceivedMsg = findViewById(R.id.textViewReceivedMessage);
+        Intent intent = getIntent();
+        String msg = intent.getStringExtra("msg");
+        textViewReceivedMsg.setText(msg);
     }
 }
