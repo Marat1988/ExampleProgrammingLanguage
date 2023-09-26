6d8dcdddfc613712f0b1f2c8731a5e2c65b20b2e
Marat <108996479+Marat1988@users.noreply.github.com>
2023-09-26 11:27:01 +0200

    UPDATE

---
+++ JavaAndroid/StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textViewTimer;
+
+    private int seconds = 0;
+    private boolean isRunning = false;
+    private boolean wasRunning = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textViewTimer = findViewById(R.id.textViewTimer);
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            isRunning = savedInstanceState.getBoolean("isRunning");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
+        }
+        runTimer();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("seconds", seconds);
+        outState.putBoolean("isRunning", isRunning);
+        outState.putBoolean("wasRunning", wasRunning);
+    }
+
+    /*@Override
+    protected void onStop() {
+        super.onStop();
+        wasRunning = isRunning;
+        isRunning = false;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        isRunning = wasRunning;
+    }*/
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        wasRunning = isRunning;
+        isRunning = false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        isRunning = wasRunning;
+
+    }
+
+    public void onClickStartTimer(View view) {
+        isRunning = true;
+    }
+
+    public void onClickStopTimer(View view) {
+        isRunning = false;
+    }
+
+    public void onClickResetTimer(View view) {
+        isRunning = false;
+        seconds = 0;
+    }
+
+    private void runTimer() {
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, secs);
+                textViewTimer.setText(time);
+
+                if (isRunning) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+
+
+    }
+}
