a6e33ec25465721ba1204c464fc73d55e0426b10
Marat <pirat03071988@mail.ru>
2023-10-14 17:15:23 +0200

    UPDATE

---
+++ JavaAndroid/ChatApp3/app/src/main/java/com/example/chatapp3/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.chatapp3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        db = FirebaseFirestore.getInstance();
+    }
+}
---
+++ JavaAndroid/Chatapp3_1/app/src/main/java/com/example/chatapp3/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.chatapp3;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseFirestore db;
+    private FirebaseAuth mAuth;
+    private RecyclerView recyclerViewMessages;
+    private MessagesAdapter adapter;
+    private EditText editTextMessage;
+    private ImageView imageViewSendMessage;
+    private String author;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        recyclerViewMessages = findViewById(R.id.recyclerViewMessages);
+        editTextMessage = findViewById(R.id.editTextMessage);
+        imageViewSendMessage = findViewById(R.id.imageViewSendMessage);
+        adapter = new MessagesAdapter();
+        recyclerViewMessages.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewMessages.setAdapter(adapter);
+
+        author = "Андрей";
+        imageViewSendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
+        db.collection("messages")
+                .orderBy("date")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        if (value != null) {
+                            List<Message> messages = value.toObjects(Message.class);
+                            adapter.setMessages(messages);
+                        }
+                    }
+                });
+        if (mAuth.getCurrentUser() != null) {
+            Toast.makeText(this, "Logged", Toast.LENGTH_SHORT).show();
+        } else {
+            signOut();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.item_signOut) {
+            mAuth.signOut();
+            signOut();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void sendMessage() {
+        String textOfMessage = editTextMessage.getText().toString().trim();
+        if (!textOfMessage.isEmpty()) {
+            recyclerViewMessages.scrollToPosition(adapter.getItemCount() - 1);
+            db.collection("messages")
+                    .add(new Message(author, textOfMessage, System.currentTimeMillis()))
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            editTextMessage.setText("");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(MainActivity.this, "Сообщение не отправлено", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
+
+    private void signOut(){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+}
