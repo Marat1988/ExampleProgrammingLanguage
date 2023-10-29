ff78ce683ec335cfe281e61088b87859180bdfa7
Marat <pirat03071988@mail.ru>
2023-10-29 23:19:55 +0200

    UPDATE

---
+++ C#/Console/Enumerable/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Progression progression = new Progression(100);
+            foreach (int i in progression)
+            {
+                Console.WriteLine(i);
+            }*/
+
+            /*StoreCollection collection = new StoreCollection(@"C:\test\test.txt");
+            foreach (int i in collection)
+            {
+                Console.WriteLine(i);
+            }*/
+
+            /*List<int> lst = new List<int>();
+            lst.Add(1);
+            lst.Add(5);
+            lst.Add(6);
+            Console.WriteLine(lst[2]);*/
+
+            /*Dictionary<int, Person> persons = new Dictionary<int, Person>();
+
+            Person person1 = new Person() { Id = 1, Name = "Петр" };
+            Person person2 = new Person() { Id = 2, Name = "Василий" };
+
+            persons.Add(person1.Id, person1);
+            persons.Add(person2.Id, person2);
+
+            Person searchPerson;
+            bool isExist = persons.TryGetValue(2, out searchPerson);
+
+            if (isExist)
+                Console.WriteLine(searchPerson.Name);*/
+
+            int[] arr = new int[5];
+            IList<int> lst = arr;
+            lst.Add(100);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/Console/Enumerable/StoreCollection.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerable
+{
+    public class StoreCollection : ICollection<int>
+    {
+
+        private readonly string _filePath;
+
+
+        public StoreCollection(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        private string[] GetNumbers()
+        {
+            string line = File.ReadAllText(_filePath);
+            string[] numbers = line.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+            return numbers;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            string[] numbers = GetNumbers();
+            foreach (string number in numbers)
+            {
+                yield return Int32.Parse(number);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(int item)
+        {
+            string[] numbers = GetNumbers();
+            if (numbers.Length == 0)
+                File.WriteAllText(_filePath, item.ToString());
+            else
+                File.AppendAllText(_filePath, "," + item.ToString());
+        }
+
+        public void Clear()
+        {
+            File.WriteAllText(_filePath, "");
+        }
+
+        public bool Remove(int item)
+        {
+            string[] numbers = GetNumbers();
+            string line = File.ReadAllText(_filePath);
+            int symbolPosition = 0;
+            foreach (string number in numbers)
+            {
+                if (Int32.Parse(number) == item)
+                {
+                    if (numbers.Length == 1)
+                    {
+                        line = "";
+                    }
+                    else if (symbolPosition == 0)
+                    {
+                        line = line.Substring(symbolPosition + number.Length + 1);
+                    }
+                    else
+                    {
+                        line = line.Remove(symbolPosition - 1, number.Length + 1);
+                    }
+                    File.WriteAllText(_filePath, line);
+                    return true;
+                }
+                symbolPosition += number.Length + 1;
+            }
+            return false;
+        }
+
+        public bool Contains(int item)
+        {
+            string[] numbers = GetNumbers();
+            foreach (string number in numbers)
+            {
+                if (Int32.Parse(number) == item)
+                    return true;
+            }
+            return false;
+        }
+
+        public void CopyTo(int[] array, int arrayIndex)
+        {
+            string[] numbers = GetNumbers();
+            foreach (string number in numbers)
+            {
+                array[arrayIndex] = Int32.Parse(number);
+                arrayIndex++;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                string[] numbers = GetNumbers();
+                return numbers.Length;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
