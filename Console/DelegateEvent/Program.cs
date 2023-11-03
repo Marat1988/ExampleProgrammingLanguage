9475f69aedf88f6223c6d013e3c60c38d802d95c
Marat <108996479+Marat1988@users.noreply.github.com>
2023-11-03 09:06:26 +0200

    UPDATE

---
+++ Console/DelegateEvent/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DelegateEvent.Student;
+
+namespace DelegateEvent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringHelper helper = new StringHelper();
+            CountDelegate d1 = helper.GetCount;
+            CountDelegate d2 = helper.GetCountSymbolA;
+
+            string testString = "LAMP";
+            Console.WriteLine("Общее количество символов: {0}", TestDelegate(d1, testString));
+            Console.WriteLine("Количество символов A: {0}", TestDelegate(d2, testString));
+
+            Student student = new Student();
+            student.Moving += Student_Moving;
+            student.Move(7);
+            Console.ReadLine();
+        }
+
+        private static void Student_Moving(object sender, MovingEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        static int TestDelegate(CountDelegate method, string testString)
+        {
+            return method(testString);
+        }
+
+        static void Show(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ MVP/MVPSample/Models/Rectangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVPSample.Models
+{
+    public class Rectangle
+    {
+        public double Length { get; set; }
+        public double Breadth { get; set; }
+
+        public double CaclulateArea()
+        {
+            return Length * Breadth;
+        }
+    }
+}
